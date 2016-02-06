8c6e1cf2653d20672543e76fe72fa3c38950074c
Henry Dinh <Henry Dinh>
2016-02-06 11:55:59 -0600

    Add data sets and main python file

---
+++ DecisionTree.py
@@ -0,0 +1,20 @@
+#Henry Dinh - HxD130130
+#CS 6375.001
+#Assignment 1 - Decision Tree
+#To test the program...
+
+#Calculates variance impurity
+def calcVI():
+    pass
+
+#Calculates information gain
+def calcIG():
+    pass
+
+def printText(x):
+    print x
+
+def main():
+    printText("Hello, what's uP?")
+
+main()
